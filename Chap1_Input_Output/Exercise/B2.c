0f63a31ea2a9c59a0ea40525583f75e0a8278791
KDS-Creator <khuatdangson2002@gmail.com>
2023-08-17 21:29:05 +0700

    Adding B2.c

---
+++ Chap1_Input_Output/Exercise/B2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int d, r;
+    printf("Nhap chieu dai: "); scanf("%d", &d);
+    printf("Nhap chieu rong: "); scanf("%d", &r);
+    printf("Dien tich hcn: %d\n", d*r);
+    return 0;
+}
