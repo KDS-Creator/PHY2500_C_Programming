c988c61d274173edd60f5b0fcb2b07ed6bf21a24
KDS-Creator <khuatdangson2002@gmail.com>
2023-08-17 21:17:13 +0700

    adding VD7.c

---
+++ Chap1_Input_Output/VD7.c
@@ -0,0 +1,12 @@
+// Nhap du lieu cho nhieu bien
+#include <stdio.h>
+
+int main() {
+    int x;
+    float y;
+    printf("Nhap gia tri cua x: ");
+    scanf("%d", &x);
+    printf("Nhap gia tri cua y: ");
+    scanf("%f", &y);
+    return 0;
+}
