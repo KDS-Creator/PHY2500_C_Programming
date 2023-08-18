22ed4a93d0b4689f8b1c890ac4abd9bb24e8cb41
KDS-Creator <khuatdangson2002@gmail.com>
2023-08-18 15:15:54 +0700

    Add VD1.c Chap2B

---
+++ Chap2B_Control_Flow_Part2/Examples/Vd1.c
@@ -0,0 +1,13 @@
+/*===== In ra 10 lần dòng chữ “Hello, World!” =====*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    for(i = 1; i <= 10; i++) {
+        printf("hello World!\n");
+    }
+    printf("\n=====================\n");
+    printf("Ket thuc chuong trinh\n");
+    return 0;
+}
